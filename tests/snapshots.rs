c52d19d7168a37d95f6fc05e2b04c572e81456b2
CAD97 <cad97@cad97.com>
2022-03-29 18:15:54 -0500

    Improve recorded snapshot format

--- tests/snapshots.rs
+++ tests/snapshots.rs
@@ -1,58 +1,16 @@
 use {
     miette::Diagnostic,
     std::{fmt, fs, path::Path},
-    thiserror::Error,
     tracing_filter::{legacy, simple},
 };
-
-#[derive(Debug, Error)]
-#[error(transparent)]
-struct StripDiagnosticUrl<'a>(&'a dyn Diagnostic);
-
-impl Diagnostic for StripDiagnosticUrl<'_> {
-    fn code<'a>(&'a self) -> Option<Box<dyn fmt::Display + 'a>> {
-        self.0.code()
-    }
-
-    fn severity(&self) -> Option<miette::Severity> {
-        self.0.severity()
-    }
-
-    fn help<'a>(&'a self) -> Option<Box<dyn fmt::Display + 'a>> {
-        self.0.help()
-    }
-
-    fn url<'a>(&'a self) -> Option<Box<dyn fmt::Display + 'a>> {
-        None
-    }
-
-    fn source_code(&self) -> Option<&dyn miette::SourceCode> {
-        self.0.source_code()
-    }
-
-    fn labels(&self) -> Option<Box<dyn Iterator<Item = miette::LabeledSpan> + '_>> {
-        self.0.labels()
-    }
-
-    fn related<'a>(&'a self) -> Option<Box<dyn Iterator<Item = &'a dyn miette::Diagnostic> + 'a>> {
-        self.0.related().map(
-            |iter| -> Box<dyn Iterator<Item = &'a dyn miette::Diagnostic> + 'a> {
-                Box::new(
-                    // leakage is sad but required to strip URLs from the test output [zkat/miette#136]
-                    iter.map(|d| -> &dyn Diagnostic { Box::leak(Box::new(StripDiagnosticUrl(d))) }),
-                )
-            },
-        )
-    }
-}
 
 struct DisplayDiagnostic<'a>(&'a dyn Diagnostic);
 
 impl fmt::Display for DisplayDiagnostic<'_> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         miette::GraphicalReportHandler::new_themed(miette::GraphicalTheme::unicode_nocolor())
-            .with_links(false)
-            .render_report(f, &StripDiagnosticUrl(self.0))
+            .with_urls(false)
+            .render_report(f, self.0)
             .or_else(|_| write!(f, "failed to pretty-print {:#?}", self.0))
     }
 }
@@ -61,18 +19,31 @@ impl fmt::Display for DisplayDiagnostic<'_> {
 fn snapshot_simple_filter_parser() {
     fn callback(path: &Path) {
         let src = fs::read_to_string(path).unwrap();
-        let (filter, report) = simple::Filter::parse(&src);
-
-        if let Some(filter) = filter {
-            insta::assert_snapshot!(Some("simple_filter"), format!("{filter:#?}"), &src);
-        } else {
-            insta::assert_snapshot!(Some("simple_filter"), "(compilation failed)", &src);
-        }
-        if let Some(report) = report {
-            let report = DisplayDiagnostic(&*report);
-            insta::assert_snapshot!(Some("simple_report"), format!("{report}"), &src);
-        } else {
-            insta::assert_snapshot!(Some("simple_report"), "(no warnings)", &src);
+        match simple::Filter::parse(&src) {
+            (Some(filter), Some(report)) => {
+                insta::assert_snapshot!(
+                    Some("simple"),
+                    format!("{filter}\n{}", DisplayDiagnostic(&*report)),
+                    &src
+                )
+            },
+            (Some(filter), None) => {
+                insta::assert_snapshot!(Some("simple"), format!("{filter}\n(no warnings)"), &src)
+            },
+            (None, Some(report)) => {
+                insta::assert_snapshot!(
+                    Some("simple"),
+                    format!("(compilation failed)\n{}", DisplayDiagnostic(&*report)),
+                    &src
+                )
+            },
+            (None, None) => {
+                insta::assert_snapshot!(
+                    Some("simple"),
+                    format!("(compilation failed)\n(no warnings)"),
+                    &src
+                )
+            },
         }
     }
 
@@ -83,14 +54,17 @@ fn snapshot_simple_filter_parser() {
 fn snapshot_legacy_filter_parser() {
     fn callback(path: &Path) {
         let src = fs::read_to_string(path).unwrap();
-        let (filter, report) = legacy::Filter::parse(&src);
-
-        insta::assert_snapshot!(Some("legacy_filter"), format!("{filter:#?}"), &src);
-        if let Some(report) = report {
-            let report = DisplayDiagnostic(&*report);
-            insta::assert_snapshot!(Some("legacy_report"), format!("{report}"), &src);
-        } else {
-            insta::assert_snapshot!(Some("legacy_report"), "(no warnings)", &src);
+        match legacy::Filter::parse(&src) {
+            (filter, Some(report)) => {
+                insta::assert_snapshot!(
+                    Some("legacy"),
+                    format!("{filter:#?}\n{}", DisplayDiagnostic(&*report)),
+                    &src
+                )
+            },
+            (filter, None) => {
+                insta::assert_snapshot!(Some("legacy"), format!("{filter:#?}\n(no warnings)"), &src)
+            },
         }
     }
 
